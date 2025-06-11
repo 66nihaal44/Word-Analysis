0b380fcf3e2908476967650696fd84123c4a61e1
66nihaal44 <nihaalshetty171@gmail.com>
2025-06-10 21:14:39 -0500

    Create wordanalyze.js

---
+++ wordanalyze.js
@@ -0,0 +1,7 @@
+const article = document.querySelector("article");
+if(article){
+  const text = article.textContent;
+  const countWordEx = /[^\s]+/g;
+  const wordBD = text.matchAll(countWordEx);
+  const wordCount = [...wordBD]/length;
+}
