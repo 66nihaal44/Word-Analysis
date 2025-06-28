65caecc70f17345e132f0b143304399dbdfa424e
66nihaal44 <nihaalshetty171@gmail.com>
2025-06-27 20:14:28 -0500

    Update wordanalyze.js

--- wordanalyze.js
+++ wordanalyze.js
@@ -1,10 +1,11 @@
 const article = document.querySelector("article");
 if(article){
+  const woCou = getElementById("wCount");
   const text = article.textContent;
   const countWordEx = /[^\s]+/g;
   const wordIt = text.matchAll(countWordEx);
   const wordArr = [...wordIt];
   const wordCount = wordArr.length;
-  wCount.textcontent = wordCount;
+  woCou.textcontent = wordCount;
   wordArr.sort((a, b) => a.localeCompare(b));
 }
