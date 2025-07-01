a9d131e79c1962cb50a4e68438ab8bfd75c9b886
66nihaal44 <nihaalshetty171@gmail.com>
2025-06-30 20:28:13 -0500

    Update wordanalyze.js

--- wordanalyze.js
+++ wordanalyze.js
@@ -1,7 +1,7 @@
 const article = document.querySelector("article");
 const text = article.textContent;
+const woCou = document.getElementById("wCount");
 if(article){
-  const woCou = document.getElementById("wCount");
   const countWordEx = /[^\s]+/g;
   const wordIt = text.matchAll(countWordEx);
   const wordArr = [...wordIt];
