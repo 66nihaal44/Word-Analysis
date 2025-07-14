920e1aca1f9b5a7cc2be473164f624e44a1b3a8e
66nihaal44 <nihaalshetty171@gmail.com>
2025-07-13 22:03:32 -0500

    Update wordanalyze.js

--- wordanalyze.js
+++ wordanalyze.js
@@ -1,15 +1,17 @@
 const woCou = document.getElementById("wCount");
 function dispWordCount(article){
-  if (!article){
-    console.log("no article");
-    return;
+  if (article){
+    const text = article.textContent;
+    const countWordEx = /[^\s]+/g;
+    const wordIt = text.matchAll(countWordEx);
+    const wordArr = [...wordIt];
+    const wordCount = wordArr.length;
+    woCou.textcontent = wordCount;
+    wordArr.sort((a, b) => a.localeCompare(b));
   }
-  const text = article.textContent;
-  const countWordEx = /[^\s]+/g;
-  const wordIt = text.matchAll(countWordEx);
-  const wordArr = [...wordIt];
-  const wordCount = wordArr.length;
-  woCou.textcontent = wordCount;
-  wordArr.sort((a, b) => a.localeCompare(b));
+  else{
+    console.log("no article");
+  }
+  return;
 }
 dispWordCount(document.querySelector("article"));
