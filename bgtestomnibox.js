6b1ff1660b2f31a13b34d2f9182b1def9af488b5
66nihaal44 <nihaalshetty171@gmail.com>
2025-08-02 20:44:37 -0500

    Update bgtestomnibox.js

--- bgtestomnibox.js
+++ bgtestomnibox.js
@@ -1,7 +1,7 @@
 console.log("bgtestomnibox.js");
 chrome.runtime.onInstalled.addListener(({reason}) => {
   if(reason === 'install'){
-    chome.storage.local.set({
+    chrome.storage.local.set({
       apiSuggestions: ['tabs', 'storage', 'scripting']
     });
   }
