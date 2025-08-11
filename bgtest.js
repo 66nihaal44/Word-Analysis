5316ae659c8d5eadda35646758123961650eb910
66nihaal44 <nihaalshetty171@gmail.com>
2025-08-10 21:43:10 -0500

    Update bgtest.js

--- bgtest.js
+++ bgtest.js
@@ -3,6 +3,7 @@ function createContextMenu(){
     id: "openPanel",
     title: "Analyze",
     contexts: ['selection']
+  })
 }
 chrome.runtime.onInstalled.addListener(() => {
     createContextMenu();
