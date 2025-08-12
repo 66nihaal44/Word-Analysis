3bd3b34d5793461866437b9e9a63f148acaf7791
66nihaal44 <nihaalshetty171@gmail.com>
2025-08-11 21:06:40 -0500

    Update testpanel.js

--- testpanel.js
+++ testpanel.js
@@ -1,8 +1,13 @@
 console.log("testpanel.js");
 function showText(text){
   if(text){
+    const countWordEx = /[^\s]+/g;
+    const wordIt = text.matchAll(countWordEx);
+    const wordArr = [...wordIt];
+    const wordCount = wordArr.length;
+    wordArr.sort((a, b) => a.localeCompare(b));
     console.log("showText function called.");
-    document.querySelector(`text`).innerText = text;
+    document.querySelector(`text`).innerText = wordCount;
   }
 }
 chrome.storage.session.get('hlText', ({hlText}) => {
