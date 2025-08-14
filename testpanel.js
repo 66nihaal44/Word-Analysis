730318ed0eb6adcf93818c4272264ea48db5d044
66nihaal44 <nihaalshetty171@gmail.com>
2025-08-13 21:40:11 -0500

    Update testpanel.js

--- testpanel.js
+++ testpanel.js
@@ -1,6 +1,5 @@
 console.log("testpanel.js");
 //const frqList = document.querySelector(`#frqList`);
-const dispWords = [], dispFrq = []
 function showText(text){
   console.log("showText function called.");
   if(text){
@@ -11,7 +10,7 @@ function showText(text){
     const wordIt = text.matchAll(countWordEx);
     const wordArr = [...wordIt];
     const wordCount = wordArr.length;
-    const frq = [];
+    const frq = [], dispWords = [], dispFrq = [];
     wordArr.sort();
     for(var i = 0, j = 0; i < wordCount; ++i, ++j){
       frq[j] = [wordArr[i][0], 1];
@@ -26,6 +25,30 @@ function showText(text){
       dispWords[i] = frq[i][0];
       dispFrq[i] = frq[i][1];
     }
+    new Chart("frqChart", {
+      type: "bar",
+      data: {
+        labels: dispWords,
+        datasets: [{
+          backgroundColor: "red",
+          data: dispFrq
+        }]
+      },
+      options: {
+        legend: {display: false},
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        },
+        title: {
+          display: true,
+          text: "Frequency of Words"
+        }
+      }
+    });
     /*for(var i = 0; i < frq.length; ++i){
       newFrq = document.createElement('li');
       newFrq.innerText = frq[i][0] + ": ";
