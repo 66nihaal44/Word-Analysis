c103c683fc2878f5e6448192dbecbbbf3b88a41c
66nihaal44 <nihaalshetty171@gmail.com>
2025-08-25 21:35:58 -0500

    Update and rename smplscrp.js to testcode.js

---
+++ testcode.js
@@ -0,0 +1,9 @@
+document.getElementById('wordNum').addEventListener('click', function() {
+  chrome.runtime.sendMessage();
+});
+document.getElementById('wordNum').addEventListener('click', function() {
+  chrome.runtime.sendMessage();
+});
+document.getElementById('wordNum').addEventListener('click', function() {
+  chrome.runtime.sendMessage();
+});
