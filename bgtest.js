a18fcf4f01c80b0b424c2313ea1f56320cae5dc9
66nihaal44 <nihaalshetty171@gmail.com>
2025-08-26 21:00:55 -0500

    Update bgtest.js

--- bgtest.js
+++ bgtest.js
@@ -12,5 +12,3 @@ chrome.contextMenus.onClicked.addListener((data, tab) =>  {
   chrome.storage.session.set({hlText: data.selectionText})
   chrome.sidePanel.open({tabId: tab.id});
 });
-chrome.runtime.onMessage.addListener(() => {
-});
