2140098d196288a312e3a6f6666a8609f15622c3
66nihaal44 <nihaalshetty171@gmail.com>
2025-08-26 21:31:08 -0500

    Update testcode.js

--- testcode.js
+++ testcode.js
@@ -1,15 +1,18 @@
 document.getElementById('excFunc').addEventListener('click', function() {
   chrome.runtime.sendMessage({
-    action: 'excFunc'
+    action: 'excFunc',
+    data: document.getElementById('excFunc').checked
   });
 });
 document.getElementById('excNum').addEventListener('click', function() {
   chrome.runtime.sendMessage({
-    action: 'excNum'
+    action: 'excNum',
+    data: document.getElementById('excNum').checked
   });
 });
-document.getElementById('wordNum').addEventListener('click', function() {
+document.getElementById('wordNum').addEventListener('change', function() {
   chrome.runtime.sendMessage({
-    action: 'wordNum'
+    action: 'wordNum',
+    data: document.getElementById('wordNum').value
   });
 });
