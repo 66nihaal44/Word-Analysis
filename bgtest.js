065d448603cf06b6a0ce4b23e2d6db4bdc8216a7
66nihaal44 <nihaalshetty171@gmail.com>
2025-09-03 21:14:23 -0500

    Update bgtest.js

--- bgtest.js
+++ bgtest.js
@@ -13,12 +13,12 @@ function createContextMenu(){
 chrome.runtime.onInstalled.addListener(() => {
     createContextMenu();
 });
-chrome.contextMenus.onClicked.addListener((item, data, tab) =>  {
-  chrome.storage.session.set({hlText: data.selectionText})
-  if(item.MenuItemId === "openWordPanel"){
+chrome.contextMenus.onClicked.addListener((info, tab) =>  {
+  chrome.storage.session.set({hlText: info.selectionText})
+  if(info.MenuItemId === "openWordPanel"){
     chrome.sidePanel.setOptions({path: 'testPanel.html'});
   }
-  else if(item.MenuItemId === "openSentPanel"){
+  else if(info.MenuItemId === "openSentPanel"){
     chrome.sidePanel.setOptions({path: 'sentPanel.html'});
   }
   chrome.sidePanel.open({tabId: tab.id});
