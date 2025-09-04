6e98b30da58917c44dd2b4cc61c0784fa44c7113
66nihaal44 <nihaalshetty171@gmail.com>
2025-09-03 21:28:52 -0500

    Create sentpanel.js

---
+++ sentpanel.js
@@ -0,0 +1,20 @@
+var sentIte;
+var sentArr;
+function calcSent(text){
+  sentIte = text.matchAll(/[.]+/g);
+  sentArr = [...sentIte];
+  for(var i = 0; i < sentArr.length; ++i){
+    const wordSentIte = sentArr[i].matchAll(/[^\s.,\/#!$%\^&\*;:{}=\-_`~()]+/g);
+    const wordSentArr = [...wordSentIte];
+    sentArr[i] = wordSentArr.count;
+  }
+  sentArr.sort(function(a, b){return b - a});
+}
+chrome.storage.session.get('hlText', ({hlText}) => {
+  calcSent(hlText);
+});
+chrome.storage.session.onChanged.addListener((changes) => {
+  if(changes['hlText']){
+    calcSent(changes['hlText'].newValue);
+  }
+})
