f942ec5b30df5c953eff7a261959dd46a2902cc3
66nihaal44 <nihaalshetty171@gmail.com>
2025-09-06 20:41:45 -0500

    Update sentpanel.js

--- sentpanel.js
+++ sentpanel.js
@@ -1,10 +1,10 @@
 var sentIte;
 var sentArr;
 function calcSent(text){
-  sentIte = text.matchAll(/[.]+/g);
+  sentIte = text.matchAll(/[^.]+/g);
   sentArr = [...sentIte];
   for(var i = 0; i < sentArr.length; ++i){
-    const wordSentIte = sentArr[i].matchAll(/[^\s.,\/#!$%\^&\*;:{}=\-_`~()]+/g);
+    const wordSentIte = sentArr[i][0].matchAll(/[^\s.,\/#!$%\^&\*;:{}=\-_`~()]+/g);
     const wordSentArr = [...wordSentIte];
     sentArr[i] = wordSentArr.count;
   }
