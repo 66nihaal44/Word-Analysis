24b0fa56be734936f1a4e5e1c445c729e4369d32
66nihaal44 <nihaalshetty171@gmail.com>
2025-10-31 21:09:07 -0500

    Update bg.js

--- bg.js
+++ bg.js
@@ -24,3 +24,20 @@ chrome.contextMenus.onClicked.addListener((info, tab) =>  {
   }
   chrome.sidePanel.open({tabId: tab.id});
 });
+function getNewlines(){
+  const slc = window.getSelection();
+  if(!slc || slc.rangeCount === 0) return "";
+  const range = slc.getRangeAt(0);
+  const fragment = range.cloneContents();
+  const walker = document.createTreeWalker(fragment, NodeFilter.SHOW_ELEMENT | NodeFiler.SHOW_TEXT);
+  let node, output = "";
+  const blockTags = new SET(["DIV", "BR", "P", "LI", "SECTION", "ARTICLE", "H1", "H2", "H3", "H4", "H5", "H6"]);
+  while((node = walker.nextNode())){
+    if(node.nodeType === Node.TEXT_NODE){
+      output += node.nodeValue;
+    } else if(node.nodeType === Node.ELEMENT_NODE && blockTags.has(node.nodeName)) {
+      output += "\n";
+    }
+  }
+  return output;
+}
