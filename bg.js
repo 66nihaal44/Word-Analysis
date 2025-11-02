68f3fa1b10bea3b7c750431ec727e7f0bb94ccb0
66nihaal44 <nihaalshetty171@gmail.com>
2025-11-01 21:02:22 -0500

    Update bg.js

--- bg.js
+++ bg.js
@@ -22,11 +22,11 @@ chrome.contextMenus.onClicked.addListener((info, tab) =>  {
   else if(info.menuItemId === "openSentPanel"){
     chrome.scripting.executeScript({
       target: {tabId: tab.id},
-      func getNewlines
+      func: getNewlines
     }, (results) => {
-      const hlText = results[0].results;
-    )}
-  chrome.storage.session.set({hlText})
+      const hlText = results[0].result;
+      chrome.storage.session.set({hlText});
+    });
     chrome.sidePanel.setOptions({path: 'sentpanel.html'});
   }
   chrome.sidePanel.open({tabId: tab.id});
@@ -36,9 +36,9 @@ function getNewlines(){
   if(!slc || slc.rangeCount === 0) return "";
   const range = slc.getRangeAt(0);
   const fragment = range.cloneContents();
-  const walker = document.createTreeWalker(fragment, NodeFilter.SHOW_ELEMENT | NodeFiler.SHOW_TEXT);
+  const walker = document.createTreeWalker(fragment, NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT);
   let node, output = "";
-  const blockTags = new SET(["DIV", "BR", "P", "LI", "SECTION", "ARTICLE", "H1", "H2", "H3", "H4", "H5", "H6"]);
+  const blockTags = new Set(["DIV", "BR", "P", "LI", "SECTION", "ARTICLE", "H1", "H2", "H3", "H4", "H5", "H6"]);
   while((node = walker.nextNode())){
     if(node.nodeType === Node.TEXT_NODE){
       output += node.nodeValue;
