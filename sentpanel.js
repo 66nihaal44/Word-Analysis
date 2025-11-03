da95414250888d889b82bd44f05da96208564628
66nihaal44 <nihaalshetty171@gmail.com>
2025-11-02 18:29:38 -0600

    Update sentpanel.js

--- sentpanel.js
+++ sentpanel.js
@@ -35,6 +35,7 @@ function calcSent(text){
   chartLabels = new Array(disArr.length).fill("name");
   Chart.defaults.global.defaultFontFamily = "Arial, sans-serif";
   Chart.defaults.global.defaultFontColor = "black";
+  Chart.defaults.global.defaultFontWeight = "lighter";
   new Chart(document.getElementById("frqChart").getContext('2d'), {
     type: "line",
     data: {
