20c34b0300fbf92706f764c5f5a62cec503c3f23
66nihaal44 <nihaalshetty171@gmail.com>
2025-11-27 20:41:59 -0600

    Update wordpanel.js

--- wordpanel.js
+++ wordpanel.js
@@ -10,6 +10,7 @@
   let excluFu = excFunc.checked;
   let excluNu = excNum.checked;
   let numWord = wordNum.value;
+  // "the", "a", "an", "and", "of", "is", "to", "that", "this", "for"
   let functionWords = []; //incomplete list
   let frqChartChart;
   document.getElementsByClassName("controls")[0].style.display = "none";
